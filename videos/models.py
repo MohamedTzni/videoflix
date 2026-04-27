4324f76661f7a6301693b0de07102549a07a4835
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-04-27 09:15:00 +0200

    add video model

---
+++ videos/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Video(models.Model):
+    """Stores all information about a single video including its file and thumbnail."""
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to='thumbnails/')
+    category = models.CharField(max_length=100)
+    video_file = models.FileField(upload_to='videos/original/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        """Returns the video title as its string representation."""
+        return self.title
